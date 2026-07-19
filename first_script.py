232c88144bac2dffc8f21f89413999a48c1e89cf
dignavora8233-sketch <dignavora8233@gmail.com>
2026-07-19 02:12:38 -0700

    Add files via upload

---
+++ first_script.py
@@ -0,0 +1,13 @@
+
+
+print("Name  : Digna Vora")
+print("Age   : 18")
+print("School: Vidhyadeep University")
+
+
+
+for i in range(1, 4):
+    print("*" * i)
+
+for i in range(2, 0, -1):
+    print("*" * i)
